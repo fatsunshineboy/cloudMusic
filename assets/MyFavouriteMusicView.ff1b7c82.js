de0ebec0f786438c175fde802634e7d669d2400c
fatsunshineboy <96576004+fatsunshineboy@users.noreply.github.com>
2022-11-27 14:52:34 +0000

    Deploying to gh-pages from @ fatsunshineboy/cloudMusic@1dc1e606e35e0d55155e4b8917a625d15400b6da 🚀

---
+++ assets/MyFavouriteMusicView.ff1b7c82.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index.3ee8ac04.js";const r={};function t(n,s){return c(),o("div",null,"\u6211\u559C\u6B22\u7684\u97F3\u4E50")}const _=e(r,[["render",t]]);export{_ as default};
